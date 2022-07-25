d74c39c0f4180f2fb1b5ca902d0eafa28a1695f9
Patrick Hastings <phastings82@gmail.com>
2022-07-25 15:09:59 -0400

    change album name, add song to ablum, delete album

--- frontend-spa/js/home.js
+++ frontend-spa/js/home.js
@@ -6,13 +6,13 @@ export default function home(albums) {
             return `
             <div class="album">
                 <h2 class="album-title">${album.title}</h2>
-                <input type="hidden" class="id field" value="${album.id}">
+                <input type="hidden" class="id_field" value="${album.id}">
                 <h3 class="album-song">Record Label: ${album.recordLabel}</h3>
                 <img src=${album.imgUrl}>
                 <p class = "album-rating">Ratings: ${album.ratings}</p>
 
                 <input type="text" class="update-recordLabel" placeholder="New record label">                
-                <button class="update-button">Update Tech Stack</button>
+                <button class="update-button">Update Record Label</button>
                 <button class="delete-button">Delete</button>
             </div>
             `
